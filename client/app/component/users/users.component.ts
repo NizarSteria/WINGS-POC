10463b0ebaa7b8dcbee189e5eb3c099c485cf9c9
Nizar Aouissaoui <aouissaoui.nizar@gmail.com>
2017-07-09 21:01:01 +0200

    modify users

--- client/app/component/users/users.component.ts
+++ client/app/component/users/users.component.ts
@@ -45,4 +45,53 @@ export class UsersComponent implements OnInit {
     );
   }
 
+    addUser() {
+    this.userService.addUser(this.addUserForm.value).subscribe(
+      res => {
+        const newUser = res.json();
+        this.users.push(newUser);
+        this.addUserForm.reset();
+        this.toast.setMessage('item added successfully.', 'success');
+      },
+      error => console.log(error)
+    );
+  }
+
+  enableEditing(user) {
+    this.isEditing = true;
+    this.user = user;
+  }
+
+  cancelEditing() {
+    this.isEditing = false;
+    this.user = {};
+    this.toast.setMessage('item editing cancelled.', 'warning');
+    // reload the users to reset the editing
+    this.getUsers();
+  }
+
+  editUser(user) {
+    this.userService.editUser(user).subscribe(
+      res => {
+        this.isEditing = false;
+        this.user = user;
+        this.toast.setMessage('item edited successfully.', 'success');
+      },
+      error => console.log(error)
+    );
+  }
+
+  deleteUser(user) {
+    if (window.confirm('Are you sure you want to permanently delete this item?')) {
+      this.userService.deleteUser(user).subscribe(
+        res => {
+          const pos = this.users.map(elem => elem._id).indexOf(user._id);
+          this.users.splice(pos, 1);
+          this.toast.setMessage('item deleted successfully.', 'success');
+        },
+        error => console.log(error)
+      );
+    }
+  }
+
 }
